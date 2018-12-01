0be7a8fc44bcd68785c0e7b869e53805d07efea8
Raluca Ion <raluca.ion@hotmail.com>
2018-12-01 15:22:04 +0200

    Updated text for the Wrong input line!

--- GuessTheNumber/Program.cs
+++ GuessTheNumber/Program.cs
@@ -10,6 +10,48 @@ namespace GuessTheNumber
 	{
 		static void Main(string[] args)
 		{
+			Random rng = new Random();
+			int r = rng.Next(0, 100);
+			bool correctGuess = false;
+
+
+
+			while (!correctGuess)
+
+			{
+				try
+				{
+					Console.WriteLine("I am thinking of a number between 0 and 100.\n Please guess the number I am thinking of:");
+					int x = Convert.ToInt32(Console.ReadLine());
+					//Console.WriteLine(r);
+
+					if (x == r)
+					{
+						Console.WriteLine("Congratulations! The number I was thinking was:" + r);
+						correctGuess = true;
+						Console.ReadLine();
+					}
+
+					else if (x > r)
+					{
+						Console.WriteLine("The guess was too big, the number I am thinking of is lower than " + x + ". Please guess again!");
+
+					}
+
+					else if (x < r)
+					{
+						Console.WriteLine("The guess was too low, the number I am thinking of is bigger than " + x + ". Please guess again!");
+
+					}
+
+				}
+				catch (FormatException WrongInput)
+				{
+					Console.WriteLine("Wrong input!!!");
+				}
+			}
+
+
 		}
 	}
 }
